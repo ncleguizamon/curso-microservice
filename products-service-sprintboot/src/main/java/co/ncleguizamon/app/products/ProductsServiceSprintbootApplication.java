e4c51febeaec6a3d949b370b0cdf89438c67f5d9
ncleguizamon <nleguizamon@cobiscorp.com>
2020-07-15 21:55:20 -0500

    ini setud initial

---
+++ products-service-sprintboot/src/main/java/co/ncleguizamon/app/products/ProductsServiceSprintbootApplication.java
@@ -0,0 +1,13 @@
+package co.ncleguizamon.app.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceSprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceSprintbootApplication.class, args);
+	}
+
+}
---
+++ products-service-sprintboot/src/test/java/co/ncleguizamon/app/products/ProductsServiceSprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ncleguizamon.app.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceSprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
