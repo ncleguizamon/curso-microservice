36bafb6652b9752fcce087c35e1cf6d4c8e7c2f2
ncleguizamon <nleguizamon@cobiscorp.com>
2020-08-20 22:08:47 -0500

    configure hye zuul mapping

---
+++ sprintboot-service-zuul-server/src/test/java/co/ncleguizamon/app/zuul/SprintbootServiceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ncleguizamon.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootServiceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
